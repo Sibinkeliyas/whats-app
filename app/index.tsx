a1e22e3c9ae5e5d5f23379605a2e148d777bf941
Sibin k <sibin.k@vvtsolutions.in>
2025-01-06 10:52:54 +0530

    added the home page

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,62 @@
-import { View, Image, Touchable, TouchableOpacity, Text, ScrollView } from "react-native";
+import { View, Image, Touchable, TouchableOpacity, Text, ScrollView, Alert } from "react-native";
 import React, { useState } from "react";
 import { whatsAppLogo } from "../constants/Images";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Button from "@/components/common/CustomButton";
 import CustomButton from "@/components/common/CustomButton";
 import FormField from "@/components/common/CustomForm";
+import { Redirect } from "expo-router";
 
 const Home = () => {
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitForm = () => {
+    if (!email && !password) {
+      Alert.alert("Please fill the form");
+    } else {
+      return <Redirect href="/(tabs)/explore" />;
+    }
+  };
   return (
-    <SafeAreaView className="h-full">
-      <ScrollView>
-        
+    <SafeAreaView className="h-full w-full">
+      <ScrollView className="h-full flex justify-center items-center w-full px-4" contentContainerClassName="w-full">
+        <View>
+          <Image source={whatsAppLogo} className="mx-auto  w-full" />
+          <Text className="font-medium text-[16px] mt-3 text-center">Welcome to Whatsapp</Text>
+
+          <FormField
+            className="!w-full mt-5  p-2 rounded-[4px] border-[.5px] border-[#b8b8b8]"
+            handleChange={(value) => setEmail(value)}
+            keyboardType="email-address"
+            placeHolder="Enter your email"
+            value={email}
+          />
+          <FormField
+            className="!w-full mt-5  p-2 rounded-[4px] border-[.5px] border-[#b8b8b8]"
+            handleChange={(value) => setPassword(value)}
+            keyboardType="default"
+            secureTextEntry
+            placeHolder="Enter your Password"
+            value={password}
+          />
+          <CustomButton
+            handlePress={submitForm}
+            title="Sign In"
+            className="bg-[#1DC070] max-w-[50%] px-4 py-2 rounded-xl mx-auto w-full mt-5 text-[#fff]"
+            textClassName="text-center text-[16px] font-medium text-[#fff]"
+          />
+        </View>
+        <View className="flex justify-center items-center mt-10">
+          <Text>
+            Read our <Text className="underline font-medium">Privacy Policy</Text> Tap agree and continue to accept the
+            terms of service
+          </Text>
+        </View>
+        <View className="!mt-[15%] w-full flex justify-center items-center ">
+          <Text className="text-[12px]">From</Text>
+          <Text className="text-[14px] font-medium text-gray-600">Facebook</Text>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
--- components/common/CustomForm.tsx
+++ components/common/CustomForm.tsx
@@ -1,6 +1,7 @@
-import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Image, KeyboardTypeOptions } from "react-native";
 import React, { useState } from "react";
 import Icon from "@expo/vector-icons/FontAwesome5";
+import { StyleSheet } from "nativewind";
 
 const FormField = ({
   handleChange,
@@ -11,29 +12,35 @@ const FormField = ({
   placeHolder,
   secureTextEntry = false,
 }: {
-  title: string;
+  title?: string;
   value: string;
   handleChange: (e: string) => void;
   className?: string;
-  keyboardType: string;
+  keyboardType: KeyboardTypeOptions | undefined;
   placeHolder: string;
   secureTextEntry?: boolean;
 }) => {
   const [showPassword, setShowPassword] = useState(false);
   return (
     <View className={`space-y-2 ${className}`}>
-      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
-      <View className="w-full h-16 px-4 mt-7 bg-black-100 rounded-2xl border-2  flex flex-row items-center">
+      {title && <Text className="text-base text-gray-100 font-pmedium">{title}</Text>}
+      <View
+        className="w-full h-16 px-2 mt-7 bg-black-100 rounded-2xl border-2 !flex !items-center !justify-between !flex-row"
+        style={style.formFieldView}
+      >
         <TextInput
-          className="flex-1 text-white font-psemibold text-base w-full border-none focus:!border-none border-2 focus:outline-none  h-full"
+          style={{ outline: "none" }}
+          className="flex-1 text-white text-base  w-full border-none focus:!border-none border-2 focus:!outline-none h-full"
           value={value}
           placeholder={placeHolder}
           placeholderTextColor="#7B7B8B"
           onChangeText={handleChange}
+          keyboardAppearance="default"
+          keyboardType={keyboardType}
           secureTextEntry={secureTextEntry && !showPassword}
         />
 
-        {title === "Password" && (
+        {secureTextEntry && (
           <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
             <Icon name={showPassword ? "eye-slash" : "eye"} />
           </TouchableOpacity>
@@ -44,3 +51,12 @@ const FormField = ({
 };
 
 export default FormField;
+
+const style = StyleSheet.create({
+  formFieldView: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+});
